f550c352ab2ef22e4c7b58b218fd8fbd7008a882
Djalil <dzhalil.mukashov@gmail.com>
2024-10-10 14:59:53 +0500

    add new input, add login form ui

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -4,6 +4,7 @@ import { Modal } from 'shared/ui/Modal/Modal';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { useCallback, useState } from 'react';
+import { LoginModal } from 'features/AuthByUsername';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -14,8 +15,12 @@ export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
     const [isAuthModal, setIsAuthModal] = useState(false);
 
-    const onToogleModal = useCallback(() => {
-        setIsAuthModal((prev: boolean) => !prev);
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
+    }, []);
+
+    const onShowModal = useCallback(() => {
+        setIsAuthModal(true);
     }, []);
 
     return (
@@ -24,14 +29,15 @@ export const Navbar = ({ className }: NavbarProps) => {
             <Button 
                 theme={ButtonTheme.CLEAR_INVERTED} 
                 className={cls.links}
-                onClick={onToogleModal}
+                onClick={onShowModal}
             >
                 {t('Войти')}
             </Button>
             {/* eslint-disable-next-line */}
-            <Modal isOpen={isAuthModal} onClose={onToogleModal}>
-                {t('Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, quidem?')} 
-            </Modal>
+            <LoginModal 
+                isOpen={isAuthModal}
+                onClose={onCloseModal}
+            />
 
         </div>
     );
